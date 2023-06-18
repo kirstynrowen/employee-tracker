47237c96ceb1fc6da8ef09bda7af9b98c3f0e1d1
kirstynrowen <50176404+kirstynrowen@users.noreply.github.com>
2023-06-18 16:00:08 -0700

    started schema

---
+++ db/schema.sql
@@ -0,0 +1,7 @@
+-- create database
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+--use database
+USE employees_db;
+
+--create tables
---
+++ index.js
@@ -0,0 +1,9 @@
+//import inquirer, mysql, console.table
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
+const table = require('console.table');
+//connect to port
+const PORT = process.env.PORT || 3001;
+
+
+
