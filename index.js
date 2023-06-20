f7550c2669b014f4a7e425e93a57fe39636f26bb
kirstynrowen <50176404+kirstynrowen@users.noreply.github.com>
2023-06-19 17:56:26 -0700

    added inquirer prompt question

--- index.js
+++ index.js
@@ -4,4 +4,25 @@ const table = require('console.table');
 //import db materials
 const db = require('./db');
 
-
+function homePage () {
+    inquirer
+    .prompt([
+        {
+            type: 'list',
+            name: 'homeScreen',
+            message: 'What would you like to do?',
+            choices: [
+                'View All Departments',
+                'View All Roles',
+                'View All Employees',
+                'Add a Department',
+                'Add a role',
+                'Ass an Employee',
+                'Update an Employee Role',
+            ]
+        }
+    ])
+    .then((response) => {
+        
+    })
+} 
