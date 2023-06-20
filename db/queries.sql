72a12d00247251897524a57495431e98dde223e3
kirstynrowen <50176404+kirstynrowen@users.noreply.github.com>
2023-06-19 18:53:04 -0700

    fixed view eall employees query with left join

--- db/queries.sql
+++ db/queries.sql
@@ -7,11 +7,14 @@ SELECT * FROM department
 SELECT * FROM role
 -- WHEN I choose to view all employees
 -- THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
-SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department
+SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
 FROM employee
-
+LEFT JOIN role ON employee.role_id = role.id
+LEFT JOIN department ON role.department_id = department.id
+LEFT JOIN employee manager ON employee.manager_id = manager.id
 -- WHEN I choose to add a department
 -- THEN I am prompted to enter the name of the department and that department is added to the database
+
 -- WHEN I choose to add a role
 -- THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
 -- WHEN I choose to add an employee
